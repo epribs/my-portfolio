c629c1dbb75f0c5d0e97f4e6c9d3d4fc3db202e3
epribs <ericpribramsky@gmail.com>
2024-06-26 20:24:40 -0500

    configs;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import { BlogPosts } from "@/components/posts";
+import config from "@/configs/config.json";
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        {`I'm Eric Quinn, a full-stack dev in America.`}
+        {config.headline}
       </h1>
-      <p className="mb-4">{`I focus on the entire webapp development lifecycle 
-      with javascript/typescript and SQL. I also enjoy go and python.`}</p>
+      <p className="mb-4">{config.subheadline}</p>
       <div className="my-8">
         <BlogPosts />
       </div>
