8e7770a63dc3ab0bb69ffe867a890febe6d367b4
epribs <ericpribramsky@gmail.com>
2024-07-01 20:37:33 -0500

    nav & about

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,19 +1,7 @@
 import Link from "next/link";
+import config from "@/configs/config.json";
 
-const navItems = {
-  "/": {
-    name: "home",
-  },
-  "/blog": {
-    name: "blog",
-  },
-  "/about": {
-    name: "experience",
-  },
-  "/contact": {
-    name: "contact",
-  },
-};
+const navItems = config.navigation;
 
 export function Navbar() {
   return (
@@ -24,11 +12,11 @@ export function Navbar() {
           id="nav"
         >
           <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
+            {Object.entries(navItems).map(([name, { url }]) => {
               return (
                 <Link
-                  key={path}
-                  href={path}
+                  key={name}
+                  href={url}
                   className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
                 >
                   {name}
