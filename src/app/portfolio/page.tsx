26f3e3f58d9093afb35c3c3a9ec359afbec5d9f6
epribs <ericpribramsky@gmail.com>
2024-07-09 20:54:15 -0500

    fix i

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -5,8 +5,8 @@ import { siteConfig } from "@/config/site";
 export default function Page() {
   return (
     <section>
-      <h1 className={title()}>{siteConfig.navItems[5].content?.headline}</h1>
-      <p className="mb-4">{siteConfig.navItems[5].content?.description}</p>
+      <h1 className={title()}>{siteConfig.navItems[3].content?.headline}</h1>
+      <p className="mb-4">{siteConfig.navItems[3].content?.description}</p>
       <div className="my-8">
         <Projects />
       </div>
